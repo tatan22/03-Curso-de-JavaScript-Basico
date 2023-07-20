0b69e9ebd7ced22da3c9df6c898ec794dd5b2bbd
Jhonatan Cardona Duarte <tatancarduar@gmail.com>
2023-07-20 12:38:40 -0500

    first commit

---
+++ 01-Mi-Primer-Proyecto-Node/index.js
@@ -0,0 +1,11 @@
+// este es el mensaje de bienvenida 
+console.log("Esta es la puerta de entrada al proyecto");
+/* 
+Esta es la parte intermedia de nuestro proyecto
+En esta parte tealizoamos los chequeos necesarios 
+Por ultimo vamos a  la ultima linea de saludo 
+esto tabien esta comentado 
+*/
+
+// mensaje para despedir 
+console.log("Adios")
---
+++ README.md
@@ -0,0 +1,1 @@
+# 03-Curso-de-JavaScript-Basico
